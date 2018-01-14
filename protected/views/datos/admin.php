221dd8688285b78bb14c41ae5208eac6dec84ded
root <root@carlos.carlos.ve>
2018-01-14 11:18:22 -0430

    modificacion en el admin de datos

--- protected/views/datos/admin.php
+++ protected/views/datos/admin.php
@@ -28,7 +28,7 @@ $('.search-form form').submit(function(){
 
 
 
-<?php echo CHtml::link('Busquedad avanzada','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Busqueda especifica','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
